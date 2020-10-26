61705786ba945c643bb472680e28cc67cb2a1a4e
klyatskiinikita <klyatskiinikita@gmail.com>
2020-10-26 15:23:33 +0200

    File "lab5_1.cpp" added

---
+++ lab5_1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include "windows.h"
+#include "math.h"
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    double F, a, b, c, x;
+    cout << "Введіть a: ";
+    cin >> a;
+    cout << "Введіть b: ";
+    cin >> b;
+    cout << "Введіть c: ";
+    cin >> c;
+    cout << "Введіть x: ";
+    cin >> x;
+    if (x < 1 && c != 0)
+    {
+        F = ((a * pow(x, 2)) / b) + (pow(b, 2) / c);
+    }
+    else if (x > 1 || c == 0)
+    {
+        F = (fabs(x - a) / (x - c));
+    }
+    else
+    {
+        F = (10 / pow(c, 2));
+    }
+    cout << "F = " << F << endl;
+    system("pause");
+
+    return 0;
+}
